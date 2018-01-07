dda804263cae569fa2ce0ae0793422b0ec6b3c54
KarthikRIyer <Karthik.iyer2@yandex.com>
2018-01-07 14:23:50 +0530

    Removed title from action bar

--- app/src/main/java/com/example/photoeditor/Draw_Activity.java
+++ app/src/main/java/com/example/photoeditor/Draw_Activity.java
@@ -48,17 +48,14 @@ public class Draw_Activity extends AppCompatActivity implements ColorPickerDialo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT < 16) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }else{
+
             View decorView = getWindow().getDecorView();
             // Hide the status bar.
             int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
             decorView.setSystemUiVisibility(uiOptions);
-        }
+
         setContentView(R.layout.activity_draw_);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-
 
         float targetW = getIntent().getExtras().getFloat("width");
         float targetH = getIntent().getExtras().getFloat("height");
